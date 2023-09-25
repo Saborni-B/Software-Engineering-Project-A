3fa88f7e3fa9b71964b5bae3ea5d177de605e775
HoManLai <103495104@student.swin.edu.au>
2023-09-25 14:21:35 +1000

    update, file relocate

--- Test_datetime.py
+++ Test_datetime.py
@@ -1,18 +1,25 @@
-import unittest
-from datetime import datetime, time
-import user_input
+import ipytest
+#from datetime import user_input
+import user_input.ipynb
+
+def test_addition():
+    assert add(2, 3) == 5, "Test failed: 2 + 3 should be 5"
+    assert add(-1, 1) == 0, "Test failed: -1 + 1 should be 0"
+    assert add(0, 0) == 0, "Test failed: 0 + 0 should be 0"
 
 
-class TestDateTime(unittest.TestCase):
-    def test_date(user_start_date, user_end_date):
+
+
+#class TestDateTime(unittest.TestCase):
+ #   def test_date(self):
         #if user_start_date == "%d/%m/%Y"
-        self.assertIsInstance(time_stamp,str, msg="%s not a string" %time_stamp)
-        self.assertIsInstance(datetime.strptime(time_stamp, "%d/%m/%Y"), datetime.datetime)
+  #      self.assertIsInstance(time_stamp,str, msg="%s not a string" %time_stamp)
+   #     self.assertIsInstance(datetime.strptime(time_stamp, "%d/%m/%Y"), datetime.datetime)
 
-        with self.assertRaises(ValueError):
-            user_end_date != "%d/%m/%Y"
+    #    with self.assertRaises(ValueError):
+     #       user_end_date != "%d/%m/%Y"
     
     #def test_time(user_start_time, user_end_time):
         
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+ #   unittest.main()
